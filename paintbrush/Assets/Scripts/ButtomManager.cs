c66d963ac3e285b42e43404f7bcf5c8687bddcee
AUGUSTINE <austineuwuigbe@gmail.com>
2018-02-28 14:56:22 +0200

    update

--- paintbrush/Assets/Scripts/ButtomManager.cs
+++ paintbrush/Assets/Scripts/ButtomManager.cs
@@ -7,18 +7,7 @@ public class ButtomManager : MonoBehaviour {
 
 	public void NewGameBtn (string MagicPaintBrush)
 	{
-<<<<<<< HEAD
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-	}
-	public void Quit(){
-		Debug.Log ("Quit");
-		Application.Quit ();
-	}
-
-=======
 	SceneManager.LoadScene(MagicPaintBrush);
 
 	}
->>>>>>> f4cd4ae8e52ee2b8986d99d05f8134a6be420af2
 }
