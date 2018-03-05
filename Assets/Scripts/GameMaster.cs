6ddf601ed2b5e30a142bc0ca9fdf154761e579ff
Minh Quan Nguyen <thesmartkingq@gmail.com>
2018-03-05 02:44:08 +0200

    New update
    Check out the Magic Paintbrush.exe file and give me opinion

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameMaster : MonoBehaviour {
-	
-	public int coin;
-	public Text cointcounter;
-	private int coinscoint;
-	public 
+public class GameMaster : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-		
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		cointcounter.text = coin.ToString ();
-		
-	}
+    public int coin;
+    public Text cointcounter;
+    private int coinscoint;
+    public
 
-	public void coinCollected(){
-		coin += 1;
+    // Use this for initialization
+    void Start()
+    {
 
-	}
-}
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cointcounter.text = coin.ToString();
+ 
+    }
+
+    public void coinCollected()
+    {
+        coin += 1;
+
+    }
+
+ }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void Escape()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+}
