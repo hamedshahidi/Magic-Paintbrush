c8364793004b674e596bcb2c0d01494408d09152
AUGUSTINE <austineuwuigbe@gmail.com>
2018-03-07 10:31:54 +0200

    HUH

---
+++ paintbrush/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void NewGameBtn (string level1)
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+	public void Quit (){
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+	void Update () {
+		if (Input.GetKey (KeyCode.Z)) {
+
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		} 
+	}
+}
