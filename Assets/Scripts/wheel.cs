fbf6548cb36ad439cb1b0f02b919720745f0ba83
Minh Quan Nguyen <thesmartkingq@gmail.com>
2018-03-12 20:47:14 +0200

    new version

---
+++ Assets/Scripts/wheel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class wheel : MonoBehaviour {
+
+    public float maxspeed = 500f;
+    public float minspeed = 100f;
+    public static float speed;
+    private float position = 0.0f;
+	private float initradius = 4.75f;
+	private float minradius = 4f;
+	private CircleCollider2D collider;
+
+ 
+        // Use this for initialization
+        void Start () {
+		collider = transform.GetComponent<CircleCollider2D>();
+        speed = Random.Range(minspeed, maxspeed);
+ 
+}
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (speed != 0)
+        {
+            speed *= 0.99f;
+            if (speed < 0.01f)
+            { speed = 0f; }
+            if (collider.radius > minradius)
+                collider.radius *= 0.99f;
+            else
+                collider.radius = minradius;
+            position -= speed;
+            position = position % 360;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, position), 1);
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+##Magic Paintbrush
+
+Dream Makers 
+
+2D game project
+
+Game for kids, range (3-7 years old)
+
+Game Development:
+-Unity
+-C sharp
+-SQL database
+
+
+
